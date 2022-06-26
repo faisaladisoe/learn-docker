fe8c8b9ca76104881f910ef874821a5d64a4a4e7
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2022-06-26 15:58:40 +0700

    approximately reach 50% progress

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      "id": "1",
+      "title": "Book Review: The Bear & The Nightingale"
+    },
+    {
+      "id": "2",
+      "title": "Game Review: Pokemon Brillian Diamond"
+    },
+    {
+      "id": "3",
+      "title": "Show Review: Alice in Borderland"
+    },
+    {
+      'id': '4',
+      'title': 'The Midnight Library'
+    },
+    {
+      'id': '5',
+      'title': 'The Alchemist'
+    }
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening for requests on port ${PORT}`);
+});
