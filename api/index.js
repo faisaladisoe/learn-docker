ab181a7cc69fb3dfdabb14bdc88589172d6a20fe
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2022-06-27 22:59:22 +0700

    implement docker compose

--- api/index.js
+++ api/index.js
@@ -9,16 +9,16 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.json([
     {
-      "id": "1",
-      "title": "Book Review: The Bear & The Nightingale"
+      "id": '1',
+      "title": 'Book Review: The Bear & The Nightingale'
     },
     {
-      "id": "2",
-      "title": "Game Review: Pokemon Brillian Diamond"
+      "id": '2',
+      "title": 'Game Review: Pokemon Brilliant Diamond'
     },
     {
-      "id": "3",
-      "title": "Show Review: Alice in Borderland"
+      "id": '3',
+      "title": 'Show Review: Alice in Borderland'
     },
     {
       'id': '4',
@@ -27,6 +27,14 @@ app.get('/', (req, res) => {
     {
       'id': '5',
       'title': 'The Alchemist'
+    },
+    {
+      'id': '6',
+      'title': 'One Up on Wall Street'
+    },
+    {
+      'id': '7',
+      'title': 'Common Stock and Uncommon Profit'
     }
   ]);
 });
